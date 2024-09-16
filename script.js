b2b0ec25f12bd63e21c52614f1843d40e99ac68a
livblackwell <160663523+livblackwell@users.noreply.github.com>
2024-09-17 10:04:35 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
