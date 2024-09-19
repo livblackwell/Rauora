900de3b9a6e07dc9d8293c8c03fbeaeb2534f801
livblackwell <160663523+livblackwell@users.noreply.github.com>
2024-09-19 12:18:15 +1200

    Update script.js
    updating java/closing one tab when other is opened

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.querySelectorAll('.dropdown-header').forEach(header => {
     header.addEventListener('click', function() {
         const dropdown = this.parentElement;
+        const allDropdowns = document.querySelectorAll('.dropdown');
+        
+        // Close other open dropdowns
+        allDropdowns.forEach(item => {
+            if (item !== dropdown) {
+                item.classList.remove('open');
+            }
+        });
+        
+        // Toggle the clicked dropdown
         dropdown.classList.toggle('open');
     });
 });
